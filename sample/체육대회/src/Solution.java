7fb53899112c8b8965e137d16e8530169cf8b715
dlqudtjs <dlqudtjs730@naver.com>
2023-06-05 17:05:13 +0900

    체육대회

--- sample/체육대회/src/Solution.java
+++ sample/체육대회/src/Solution.java
@@ -1,69 +1,34 @@
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.PriorityQueue;
+class Solution {
+    static boolean[] visited;
+    static int max = 0;
 
-public class Solution {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         solution(new int[][] { { 40, 10, 10 }, { 20, 5, 0 }, { 30, 30, 30 }, { 70, 0, 70 }, { 100, 100, 100 } });
     }
 
     public static int solution(int[][] ability) {
-        int answer = 0;
+        visited = new boolean[ability.length];
 
-        int row = ability.length;
-        int col = ability[0].length;
+        dfs(0, 0, ability);
 
-        Person[][] persons = new Person[col][col]; // 가장 높은 순위를 열의 개수만큼 생성
-        PriorityQueue<Person> pq = new PriorityQueue<Person>(new Comparator<Person>() {
+        return max;
+    }
 
-            @Override
-            public int compare(Person p1, Person p2) {
-                if (p1.getAbility() == p2.getAbility()) {
-                    return p2.getId() - p1.getId();
-                }
-
-                return p2.getAbility() - p1.getAbility();
-            }
-        });
-
-        for (int i = 0; i < col; i++) {
-            for (int j = 0; j < row; j++) {
-                pq.add(new Person(j, ability[j][i]));
-            }
-
-            for (int j = 0; j < row; j++) {
-                Person selectPerson = pq.poll();
-                persons[j][i] = selectPerson;
-            }
+    private static void dfs(int depth, int sum, int[][] ability) {
+        if (depth == ability[0].length) {
+            max = Math.max(max, sum);
+            return;
         }
 
-        answer = dfs(0, 0, 0, persons);
+        for (int i = 0; i < ability.length; i++) {
+            if (visited[i]) {
+                continue;
+            }
 
-        return answer;
-    }
-
-    public static int dfs(int depth, int index, int sum, Person[][] persons) {
-        // if(depth == )
-
-        return 1;
-    }
-}
-
-class Person {
-    private int id;
-    private int ability;
-
-    Person(int id, int ability) {
-        this.id = id;
-        this.ability = ability;
-    }
-
-    int getId() {
-        return id;
-    }
-
-    int getAbility() {
-        return ability;
+            visited[i] = true;
+            // depth = 종목의 깊이 i = 사람의 깊이
+            dfs(depth + 1, sum + ability[i][depth], ability);
+            visited[i] = false;
+        }
     }
 }
